337305a38c09bddac406e88b28c3bdc9395c57c6
zach89 <zach88@163.com>
2018-10-06 13:41:34 +0800

    'init'

---
+++ src/main/java/com/sf/zachweb/ZachWebApplication.java
@@ -0,0 +1,12 @@
+package com.sf.zachweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZachWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZachWebApplication.class, args);
+    }
+}
