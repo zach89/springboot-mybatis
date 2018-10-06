01c8e8898ce08133a7e7bfe5dd5524a7d4b44eb5
zach89 <zach88@163.com>
2018-10-06 21:26:37 +0800

    test

--- src/main/java/com/sf/zachweb/controller/BaseController.java
+++ src/main/java/com/sf/zachweb/controller/BaseController.java
@@ -1,5 +1,7 @@
 package com.sf.zachweb.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.sf.zachweb.services.DubboService;
 import com.sf.zachweb.utils.Res;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 @Api(value = "BaseController", description = "基本测试")
 public class BaseController {
-
+    @Reference(version = "${demo.service.version}",
+            application = "${dubbo.application.id}",
+            url = "dubbo://localhost:12345")
+    private DubboService dubboService;
     @ApiOperation(value = "hello world", notes = "hello world")
     @RequestMapping(value = "",method = RequestMethod.GET)
     public Res index() {
-        return Res.buildOk("hello world!");
+        return Res.buildOk(dubboService.test());
     }
 }
---
+++ src/main/java/com/sf/zachweb/services/DubboService.java
@@ -0,0 +1,5 @@
+package com.sf.zachweb.services;
+
+public interface DubboService {
+    String test();
+}
